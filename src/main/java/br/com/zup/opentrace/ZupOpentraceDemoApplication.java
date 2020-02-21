a16edace88eb4920dda6c15f6f8d6f30b5f433d4
vinicius.ribeiro <vinicius.ribeiro@uds.com.br>
2020-02-21 18:36:29 -0300

    Bootstrap application

---
+++ src/main/java/br/com/zup/opentrace/ZupOpentraceDemoApplication.java
@@ -0,0 +1,13 @@
+package br.com.zup.opentrace;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ZupOpentraceDemoApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(ZupOpentraceDemoApplication.class, args);
+    }
+
+}
---
+++ src/test/java/br/com/zup/opentrace/ZupOpentraceDemoApplicationTests.java
@@ -0,0 +1,13 @@
+package br.com.zup.opentrace;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ZupOpentraceDemoApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
