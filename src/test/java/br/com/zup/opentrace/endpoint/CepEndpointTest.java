2133ca58a8c60167c5e8ad6227625858113c6bf1
Vinicius Ribeiro <vinicius.aparecido@zup.com.br>
2020-02-26 10:33:40 -0300

    Fixing unit tests

--- src/test/java/br/com/zup/opentrace/endpoint/CepEndpointTest.java
+++ src/test/java/br/com/zup/opentrace/endpoint/CepEndpointTest.java
@@ -2,19 +2,13 @@ package br.com.zup.opentrace.endpoint;
 
 
 import br.com.zup.opentrace.enums.ViaCepResponses;
-import br.com.zup.opentrace.exception.InvalidCepException;
-import br.com.zup.opentrace.exception.InvalidOutputFormatException;
 import br.com.zup.opentrace.service.CepService;
 import io.micrometer.core.instrument.util.StringUtils;
-import org.junit.Rule;
 import org.junit.Test;
-import org.junit.rules.ExpectedException;
 import org.junit.runner.RunWith;
 import org.mockito.InjectMocks;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.boot.test.autoconfigure.web.servlet.AutoConfigureMockMvc;
 import org.springframework.boot.test.autoconfigure.web.servlet.WebMvcTest;
-import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.boot.test.mock.mockito.MockBean;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.MediaType;
@@ -23,20 +17,15 @@ import org.springframework.test.context.junit4.SpringRunner;
 import org.springframework.test.web.servlet.MockMvc;
 import org.springframework.test.web.servlet.MvcResult;
 import org.springframework.test.web.servlet.ResultActions;
-import org.springframework.test.web.servlet.ResultMatcher;
-import org.springframework.test.web.servlet.setup.MockMvcBuilders;
-import org.springframework.web.context.WebApplicationContext;
 
 import java.util.EnumSet;
 
 import static org.assertj.core.api.Assertions.assertThat;
-import static org.assertj.core.internal.bytebuddy.matcher.ElementMatchers.is;
 import static org.mockito.ArgumentMatchers.anyString;
 import static org.mockito.ArgumentMatchers.isNull;
 import static org.mockito.Mockito.when;
 import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.post;
 import static org.springframework.test.web.servlet.result.MockMvcResultHandlers.print;
-import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.jsonPath;
 import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
 
 @RunWith(SpringRunner.class)
@@ -64,9 +53,6 @@ public class CepEndpointTest {
             "}";
     @MockBean
     private CepService cepService;
-
-    @Rule
-    public ExpectedException exception = ExpectedException.none();
 
     @Test
     public void contextLoads() {
--- src/test/java/br/com/zup/opentrace/service/impl/CepServiceImplTest.java
+++ src/test/java/br/com/zup/opentrace/service/impl/CepServiceImplTest.java
@@ -1,39 +1,32 @@
 package br.com.zup.opentrace.service.impl;
 
+import br.com.zup.opentrace.exception.InvalidCepException;
+import br.com.zup.opentrace.exception.InvalidOutputFormatException;
 import org.junit.Before;
-import org.junit.jupiter.api.Test;
+import org.junit.Test;
 import org.junit.runner.RunWith;
 import org.mockito.InjectMocks;
-import org.mockito.junit.MockitoJUnitRunner;
-import org.springframework.beans.factory.annotation.Autowired;
+import org.mockito.Mock;
 import org.springframework.boot.test.context.SpringBootTest;
-import org.springframework.boot.test.mock.mockito.MockBean;
-import org.springframework.context.annotation.Bean;
-import org.springframework.context.support.PropertySourcesPlaceholderConfigurer;
-import org.springframework.core.env.Environment;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
-import org.springframework.test.context.ActiveProfiles;
 import org.springframework.test.context.junit4.SpringRunner;
 import org.springframework.test.util.ReflectionTestUtils;
 import org.springframework.web.client.RestTemplate;
 
-import java.util.Properties;
-
 import static org.assertj.core.api.Assertions.assertThat;
-import static org.junit.jupiter.api.Assertions.*;
 import static org.mockito.ArgumentMatchers.any;
 import static org.mockito.ArgumentMatchers.anyString;
 import static org.mockito.Mockito.when;
 
-//@RunWith(SpringRunner.class)
-//@SpringBootTest
+@RunWith(SpringRunner.class)
+@SpringBootTest
 public class CepServiceImplTest {
 
     @InjectMocks
     private CepServiceImpl cepService;
 
-    @MockBean
+    @Mock
     private RestTemplate restTemplate;
 
     public static final String VALID_ZIP_CODE = "38400386";
@@ -49,17 +42,29 @@ public class CepServiceImplTest {
             "  \"gia\": \"\"\n" +
             "}";
 
+
     @Before
-    public void setUpStatic() {
-        Properties props = System.getProperties();
-        props.setProperty("viacep.address", "http://viacep.com.br/ws/{zipCode}/");
+    public void setUp() {
+        ReflectionTestUtils.setField(cepService, "viacepAddress", "http://viacep.com.br/ws/{zipCode}/");
     }
 
-
+    @Test
     public void findByZipCode() {
         when(restTemplate.getForEntity(anyString(), any(), anyString()))
                 .thenReturn(new ResponseEntity<>(ZUP_CODE_RESPONSE, HttpStatus.OK));
         final ResponseEntity<String> response = cepService.findByZipCode(VALID_ZIP_CODE, null);
+        assertThat(response).isNotNull();
+    }
+
+    @Test(expected = InvalidCepException.class)
+    public void shouldThrowInvalidCepExceptionOnFindByZipCode() {
+        final ResponseEntity<String> response = cepService.findByZipCode("ABCDEF", null);
+        assertThat(response).isNotNull();
+    }
+
+    @Test(expected = InvalidOutputFormatException.class)
+    public void shouldThrowInvalidOutputFormatExceptionOnFindByZipCode() {
+        final ResponseEntity<String> response = cepService.findByZipCode(VALID_ZIP_CODE, "INVALID_FORMAT");
         assertThat(response).isNotNull();
     }
 }
